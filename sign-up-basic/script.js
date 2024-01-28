b8c6499b1e3f7d19d146de2046e60ef23bfc4da1
Roee Hashai <roee.hashai@gmail.com>
2024-01-28 16:09:31 +0200

    no js

--- sign-up-basic/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('inputPassword').addEventListener('focus', function () {
-    document.querySelector('.password-requirements').style.display = 'block';
-});
-
-document.getElementById('inputPassword').addEventListener('blur', function () {
-    document.querySelector('.password-requirements').style.display = 'none';
-});
